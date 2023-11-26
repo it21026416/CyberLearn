f30e13790e8991f0354c9da42867fd6f8e140df1
Rukshana <it21026416@my.sliit.lk>
2023-11-26 22:28:09 +0530

    Added URL fix

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template
-from RecommendationEngine import recommend_courses_for_article, thn_articles, cybrary_courses
+from RecommendationEngine import recommend_courses_for_article, scrape_data
 
 app = Flask(__name__)
 
-# Generate recommendations using the RecommendationEngine.py script for all articles
-recommendations_list = []
-
-for article_index, (article_title, _) in enumerate(thn_articles):
-    recommended_courses = recommend_courses_for_article(article_index)
-    recommendations_list.append((article_title, recommended_courses))
+# Get the data from the RecommendationEngine
+thn_articles, cybrary_courses = scrape_data()
 
 @app.route('/')
 def index():
-    return render_template('index.html', recommendations=recommendations_list, cybrary_courses=cybrary_courses)
+    recommendations_list = []
+    for article_index, article in enumerate(thn_articles):
+        if article_index == 7: break
+        recommended_courses = recommend_courses_for_article(article, cybrary_courses, article_index)
+        recommendations_list.append((article, recommended_courses))
+
+    return render_template('index.html', recommendations=recommendations_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
